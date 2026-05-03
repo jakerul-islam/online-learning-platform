c3adeba2024e4db3a0c79599f5df6c1129dc404d
jakerulcode <jakerulislam024@gmail.com>
2026-05-03 13:25:36 +0600

    domain name added

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://online-learning-platform-self-nine.vercel.app/data.json"
 })
 export const { signIn, signUp,signOut, useSession } = createAuthClient()
